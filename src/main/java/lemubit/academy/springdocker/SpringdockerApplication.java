5f22128bc90a949f05106651d438e5af820b9605
Ogbunude Lemuel <lemuelcco@gmail.com>
2020-07-09 01:58:10 +0100

    Add web link

--- src/main/java/lemubit/academy/springdocker/SpringdockerApplication.java
+++ src/main/java/lemubit/academy/springdocker/SpringdockerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringdockerApplication {
+//https://medium.com/holisticon-consultants/dont-build-fat-jars-for-docker-applications-6252a5571248
 
     public static void main(String[] args) {
         SpringApplication.run(SpringdockerApplication.class, args);
